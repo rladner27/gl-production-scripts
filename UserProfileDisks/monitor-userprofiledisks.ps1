dcac0b8434ab345f27973c5f1bf32ec56d43183b
smood922 <48338396+smood922@users.noreply.github.com>
2021-02-15 17:32:31 -0800

    initial commit of monitor-userprofiledisks

---
+++ UserProfileDisks/monitor-userprofiledisks.ps1
@@ -0,0 +1,9 @@
+$DisksInWarning = @()
+$VHDs = get-disk | Where-Object {$_.Location -match "VHD"}
+foreach($VHD in $VHDs){
+    $SID = [io.path]::GetFileNameWithoutExtension("$($VHD.Location)").TrimStart("UVHD-") #at least for OMC these all have "UVHD-" at the beginning of the file name. Alternatively could regex match to the standard SID format.
+    $SIDObject = New-Object System.Security.Principal.SecurityIdentifier ($SID) 
+    $Username = $SIDObject.Translate([System.Security.Principal.NTAccount])
+    $Volume = $VHD | Get-Partition | Get-Volume
+    if($Volume.SizeRemaining -lt $volume.Size * 0.10 ){ $DisksInWarning += "$($Username.Value) UPD Less than 10% remaining. Path: $($VHD.Location)"}
+}
